43a9fa5c7968ef0f73828ecd1a02cf7ed4dc7797
TruxSux <kaithomasdean@outlook.com>
2022-10-22 09:56:59 -0400

    updated WIP placeholder

--- src/PageTwo.js
+++ src/PageTwo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import BackgroundAnimation from "./BackgroundAnimation";
 
+import "./pagetwo.css"
+
 export default function PageTwo ({inc, dec}) {
     return (
         <div className="background">
             <BackgroundAnimation/>
-            This page is a work in progess and a place holder for my projects page
+            <h1 className="temp">
+                This page is a work in progess and a place holder for my projects page
+                Stand by for future updates found on my <a href="https://www.github.com/Kaiolohia/personal-website">github</a>
+            </h1>
         </div>
     )
 }
